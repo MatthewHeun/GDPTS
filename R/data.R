3c72fc322edb65cd9ec5da5031156c15b1098549
MatthewHeun <matthew.heun@me.com>
2025-06-17 17:26:54 +0100

    Update everything.

--- R/data.R
+++ R/data.R
@@ -2,7 +2,13 @@
 #'
 #' A data frame containing harmonized PWT data.
 #'
-#' @format A data frame of rgdpe data.
+#' @format A tibble with `r nrow(pwt_harmonized)` rows and `r ncol(pwt_harmonized)` columns.
+#' \describe{
+#' \item{Country}{A column of strings identifying countries by ISO3c codes.}
+#' \item{Year}{A column of integer years.}
+#' \item{pop}{A double-precision column of population in persons.}
+#' \item{rgdpe}{A double-precision column of GDP in constant 2017$.}
+#' }
 #'
 #' @examples
 #' pwt_harmonized
---
+++ tests/testthat/test-data.R
@@ -0,0 +1,3 @@
+test_that("data are correct", {
+  expect_true(!is.null(pwt_harmonized))
+})
